993ebf886421c9546bcad9438fc3563041342ce4
Gideon <g.vdherik@live.nl>
2021-07-12 07:24:59 +0200

    Added a logo and some config options
    Added a logo and some config options

--- conf.lua
+++ conf.lua
@@ -1,9 +1,9 @@
 function love.conf(t)
-    t.title = "Platformer"
+    t.title = "Platformer Game"
     t.version = "11.3"
 --    t.console = true
 --    t.window.fullscreen = true
     t.window.height = 768
     t.window.width = 1000
---    t.window.icon = "assets/sprites/zombie.png"
+    t.window.icon = "assets/sprites/logo.png"
 end
